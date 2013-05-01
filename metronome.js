51af2266709caf8257cd8507807d71314ed46cc6
wilson428 <christopher.e.wilson@gmail.com>
2013-05-01 15:15:10 -0400

    New method to access shapes

--- metronome.js
+++ metronome.js
@@ -1,5 +1,3 @@
-/*global Raphael */
-
 var metronome = function(opts) {
     //primary variables
     var l = typeof opts.len !== "undefined" ? opts.len : 200, // length of metronome arm
@@ -7,19 +5,22 @@ var metronome = function(opts) {
     	w = 2 * l * Math.cos(r),
         tick_func = typeof opts.tick !== "undefined" ? opts.tick : function() {}, //function to call with each tick
         end_func = typeof opts.complete !== "undefined" ? opts.complete : function() {}, //function to call on completion
-        path_to_audio = typeof opts.path !== "undefined" ? opts.path : "audio/", //function to call on completion
-        playSound = typeof opts.sound !== "undefined" ? opts.sound : true; //function to call on completion
+        playSound = typeof opts.sound !== "undefined" ? opts.sound : true; 
 
+	// initialize Raphael paper if need be        
     switch(typeof opts.paper) {
 		case "string": paper = Raphael(opts.paper, w, l + 20); break;
 		default: paper = Raphael(0, 0, w, l + 20); break;
     }
-    
-    // initialize audio
-    var sound = document.createElement('audio');
-    sound.setAttribute('src', path_to_audio + 'tick.wav');
-    sound.setAttribute('id', 'tick');
-    document.body.appendChild(sound);
+
+	// initialize audio if need be
+    if (playSound && opts.audio) {
+		// initialize audio
+		var sound = document.createElement('audio');
+		sound.setAttribute('src', opts.audio);
+		sound.setAttribute('id', 'tick');
+		document.body.appendChild(sound);
+    }
     
     // derivative variables
     var y0 = l * Math.cos(Math.PI * r / 180),
@@ -33,7 +34,7 @@ var metronome = function(opts) {
         'stroke-width': 0    
     });
     
-    var arm = paper.path("M" + x + "," + y + "v-" + l).attr({
+    var arm = paper.path("M" + x + "," + (y + 5) + "v-" + (l - 5)).attr({
         'stroke-width': 5,
         stroke: "#999"
     }).data("id", "arm");
@@ -83,20 +84,33 @@ var metronome = function(opts) {
             
             //2 iterations per animation * 60000 ms per minute / tempo
             var interval = 120000 / tempo;
+
+			var animationDone = function() { 
+				tick(this, repeats, done); 
+			};
+			
+            var ticktockAnimationParam = {
+                "50%": { transform:"R20 " + x + "," + y, easing: "sinoid", callback: animationDone },
+                "100%": { transform:"R-20 " + x + "," + y, easing: "sinoid", callback: animationDone }
+            };
             
             //animation            
-            var ticktock = Raphael.animation({
-                "50%": { transform:"R20 " + x + "," + y, easing: "sinoid", callback: function() { tick(this, repeats, done); }},
-                "100%": { transform:"R-20 " + x + "," + y, easing: "sinoid", callback: function() { tick(this, repeats, done); }}
-            }, interval).repeat(repeats / 2);
-            
-            arm.animate(ticktock);
-            weight.animateWith(arm, ticktock, ticktock);            
+			var ticktock = Raphael.animation(ticktockAnimationParam, interval).repeat(repeats / 2);
+			arm.animate(ticktock);
+			weight.animateWith(arm, ticktockAnimationParam, ticktock); 
         },
         stop: function() {
             mn.stop();
             mn.attr("transform", "R0 " + x + "," + y);                
             end_func();
+        },
+        shapes: function() {
+        	return {
+        		outline: outline,
+        		arm: arm,
+        		weight: weight,
+        		vertex: vertex        	
+        	}
         }
     };
 };
